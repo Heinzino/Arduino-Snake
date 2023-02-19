a5cbf841d2cea295e9233ed607bd975b1f18dd1e
Heinzino <51274202+Heinzino@users.noreply.github.com>
2023-02-19 09:53:45 -0700

    more compact initialization of screen and board

--- Code/display.ino
+++ Code/display.ino
@@ -23,14 +23,53 @@ int resetCount = 0;
 #define YELLOW 0xFFE0
 #define WHITE 0xFFFF
 
+
+// Board Varaibles
+const int BOARD_SIZE = 312;
+const int NUM_OF_TILES = 8;
+const int TILE_SIZE = BOARD_SIZE / NUM_OF_TILES;
+const int START_X = 84;
+const int START_Y = 4;
+
+int snakeMap[NUM_OF_TILES][NUM_OF_TILES];
+int snakeX[NUM_OF_TILES];
+int snakeY[NUM_OF_TILES];
+
+
+void createBoard() {
+  //Board of the snake game
+  //The board will go here, assuming 8x8
+  for (int i = 0; i < NUM_OF_TILES; i++) {
+    for (int j = 0; j < NUM_OF_TILES; j++) {
+      if((i + j) % 2 == 0) { //Black Tile 9 
+        snakeMap[i][j] = 9;
+      } else { //White Tile 10
+        snakeMap[i][j] = 10;
+      }
+    }
+  }
+}
+
+void displayBoard() {
+  //This will display the whole board on the Arduino SPI LCD
+  for (int i = 0; i < NUM_OF_TILES; i++) {
+    for (int j = 0; j < NUM_OF_TILES; j++) {
+      if(snakeMap[i][j] == 10) {//Black Tile 9
+      //White Tile 10
+        tft.drawRect(TILE_SIZE*i + START_X, TILE_SIZE*j + START_Y, TILE_SIZE, TILE_SIZE, WHITE);
+      }
+    }
+  }
+}
+
 void setup() {
 
   // Initalize the screen and set the orientation correctly, then make sure it's clear.
   tft.begin();
   tft.setRotation(1);
   tft.fillScreen(BLACK);
-  tft.drawRect(int(84), int(4),int(312),int(312), WHITE);
-
+  tft.drawRect(int(START_X), int(START_Y),int(BOARD_SIZE),int(BOARD_SIZE), WHITE);
+  displayBoard();
 
 }
 
--- Code/snake.ino
+++ Code/snake.ino
@@ -28,6 +28,9 @@ const int TILE_SIZE = BOARD_SIZE / NUM_OF_TILES;
 const int START_X = 84;
 const int START_Y = 4;
 
+const int white_tile = 10;
+const int black_tile = 9;
+
 //1 is right, 2 is up, 3 is left, 4 is down
 //7 is apple, 8 is snake, 9 is black tile, 10 is white tile
 int direction = 1;
@@ -42,16 +45,10 @@ int snakeY[NUM_OF_TILES];
 void setup() {
   Serial.begin(9600);
 
-  //Makes background black and have a green border 
-  tft.begin();
-  tft.setRotation(1); //Makes starting point at top left corner of the screen when its horizontal
-  tft.fillScreen(BLACK);
-  tft.drawRect(START_X, START_Y, BOARD_SIZE, BOARD_SIZE, BLUE);
+  Initialze_Screen_and_Board();
   
   //Starts of page
   howToPlayPage();
-  //Creation of the board
-  displayBoard();
   //Start position of snake middle of board
   snakeX[NUM_OF_TILES / 2] = 8; //Snake Head
   snakeY[NUM_OF_TILES / 2 - 1] = 8;
@@ -94,6 +91,16 @@ Button prev = {0, 0, 90, 45, "PREV", false};
 Button play = {0, 0, 100, 100, "START", false};
 Button retry = {0, 0, 100, 100, "PLAY AGAIN", false};
 
+
+void Initialize_Screen_and_Board(){
+  //Makes background black
+  tft.begin();
+  tft.setRotation(1); //Makes starting point at top left corner of the screen when its horizontal
+  tft.fillScreen(BLACK);
+  displayBoard();
+  
+}
+
 void howToPlayPage() {
   //Beginning of the page
   int pageNumber = 1;
@@ -129,9 +136,9 @@ void createBoard() {
   for (int i = 0; i < NUM_OF_TILES; i++) {
     for (int j = 0; j < NUM_OF_TILES; j++) {
       if((i + j) % 2 == 0) { //Black Tile 9 
-        snakeMap[i][j] = 9;
+        snakeMap[i][j] = black_tile;
       } else { //White Tile 10
-        snakeMap[i][j] = 10;
+        snakeMap[i][j] = white_tile;
       }
     }
   }
@@ -141,7 +148,7 @@ void displayBoard() {
   //This will display the whole board on the Arduino SPI LCD
   for (int i = 0; i < NUM_OF_TILES; i++) {
     for (int j = 0; j < NUM_OF_TILES; j++) {
-      if(snakeMap[i][j] == 9) {//Black Tile 9
+      if(snakeMap[i][j] == black_tile) {//Black Tile 9
         tft.drawRect(TILE_SIZE*i + START_X, TILE_SIZE*j + START_Y, TILE_SIZE, TILE_SIZE, BLACK);
       } else { //White Tile 10
         tft.drawRect(TILE_SIZE*i + START_X, TILE_SIZE*j + START_Y, TILE_SIZE, TILE_SIZE, WHITE);
